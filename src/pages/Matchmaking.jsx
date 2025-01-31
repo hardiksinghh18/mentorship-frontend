b2e436240490f5083c8b3d9e36a85c95143a7b39
Hardik Singh <hardiksingh950@gmail.com>
2025-01-31 12:20:38 +0530

    bug fixes

--- src/pages/Matchmaking.jsx
+++ src/pages/Matchmaking.jsx
@@ -38,10 +38,10 @@ const Matchmaking = () => {
   // Calculate the match score
   const calculateMatch = (user1, user2) => {
 
-    const matchingSkills =user1?.skills? user1?.skills.split(",").map(item=>item.trim()).filter(skill => user2?.skills?.includes(skill)).length:[];
-    const matchingInterests = user1?.interests? user1?.interests.split(",").map(item=>item.trim()).filter(interest => user2?.interests?.includes(interest)).length:[];
+    const matchingSkills = user1?.skills ? user1?.skills.split(",").map(item => item.trim()).filter(skill => user2?.skills?.includes(skill)).length : [];
+    const matchingInterests = user1?.interests ? user1?.interests.split(",").map(item => item.trim()).filter(interest => user2?.interests?.includes(interest)).length : [];
 
-    
+
     return matchingSkills + matchingInterests; // You can tweak this based on weightage you want for skills vs interests
   };
 
@@ -73,28 +73,26 @@ const Matchmaking = () => {
   return (
     <div className="min-h-screen bg-gradient-to-t flex items-center justify-center from-slate-800 to-[#000104] text-white py-12 px-6 md:px-20">
       <div className="flex items-center justify-center flex-col">
-   
+
 
         {/* Display Best Matches */}
         <div className="mt-12 w-full">
           {bestMatches.length > 0 ? (
             <div className="space-y-4">
               <h2 className="text-3xl font-semibold mb-4 text-center">Your Best Matches</h2>
-            {bestMatches.map((item) => (
-              item.matchScore > 0 ? (
-                <ProfileCard
-                  key={item.id}
-                  profile={item}
-                  currentUserId={user?.id}
-                  matchScore={item.matchScore}
+              {bestMatches.map((item) => (
+                item.matchScore > 0 ? (
+                  <ProfileCard
+                    key={item.id}
+                    profile={item}
+                    currentUserId={user?.id}
+                    matchScore={item.matchScore}
                   // onSendRequest={handleSendRequest}
-                />
-              ) : (
-                <div className='w-full h-full flex justify-center items-center'>Sorry! No matches can be found for now.</div>
-              )
-            ))}
-          </div>
-          
+                  />
+                ) : null
+              ))}
+            </div>
+
           ) : (
             <p className="text-center text-slate-400">No matches found yet. Please try again later.</p>
           )}
